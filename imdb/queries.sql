9a5fdc735a29c6cc47033774f706d346b57bb3c8
nTudor0911 <40658894+ntudor11@users.noreply.github.com>
2019-05-23 17:56:41 +0200

    imdb queries

--- imdb/queries.sql
+++ imdb/queries.sql
@@ -7,11 +7,19 @@ select count(*) from person where height is null; -- result: 47315
 
 -- 1. b. In the database, there are 365 movies where the average height of all people involved is less than 165 centimetres (ignoring people where the height is not registered). For how many movies is the average height of all people involved greater than 190 centimetres?
 
-select * from person
-group by id having avg(height) < 165;
+select count(*) from (
+  select movieId, avg(P.height)
+    from Involved I
+       join person P on I.personId = P.ID
+    group by I.movieId
+    having avg(P.height) < 165
+  ) X; -- test
 
-select distinct i.movieId, p.name
-from involved i right join person p
-having avg(p.height) < 165
-on i.personId = p.id
-;
+select count(*) from (
+  select movieId, avg(p.height)
+    from involved i
+      join person as p
+      on i.personId = p.id
+    group by i.movieId
+    having avg(p.height) > 190
+) x; -- result 190
