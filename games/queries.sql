3cbe565c8099301c6cf9a79cbac17aac3d40077b
nTudor0911 <40658894+ntudor11@users.noreply.github.com>
2019-05-28 15:12:55 +0200

    querying games

--- games/queries.sql
+++ games/queries.sql
@@ -22,3 +22,31 @@ select count(*) from (
   left join Achievement a on pa.AchievementId = a.id
     where a.id is null
 ) x; -- result 2
+
+
+-- 1. d. Only 5 distinct players have both a registered score and a registered achievement in some game produced by “Electronix Arts”. How many distinct players have both a registered score and a registered achievement in some game produced by “Codemasters”?
+
+select count(distinct PA.playerId)
+  from PlayerAchievement PA
+    join Achievement A on PA.achievementId = A.id
+    join Game G on A.gameId = G.id
+    join Score S on PA.playerId = S.playerId and S.gameId = G.id
+  where G.producer = 'Electronix Arts'; -- test, result 5
+
+select count(distinct pa.playerId)
+  from PlayerAchievement pa
+    join Achievement a on pa.achievementId = a.id
+    join Game g on a.gameId = g.id
+    join Score s on pa.playerId = s.playerId and s.gameId = g.id
+  where g.producer = 'Codemasters'; -- result 6
+
+-- 1. e. The number of distinct players who have a registered score in some game, but no registered achievement in that same game, is 493. How many distinct players have a registered achievement in some game, but no registered score in that game?
+
+select count(distinct pa.playerId)
+  from PlayerAchievement pa
+    join Achievement a on pa.achievementId = a.id
+  where not exists (
+    select * from Score s
+      where pa.playerId = s.playerId
+      and a.gameId = s.gameId
+  ); -- result 486
